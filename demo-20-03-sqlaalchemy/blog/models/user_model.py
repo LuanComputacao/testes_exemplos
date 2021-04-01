4003b730f42d625a47379e6df61c97631f7794b9
Vinicius Abade <vinicius@kenzie.com.br>
2021-04-01 20:24:06 -0300

    demo 31/03

--- demo-20-03-sqlaalchemy/blog/models/user_model.py
+++ demo-20-03-sqlaalchemy/blog/models/user_model.py
@@ -1,20 +1,32 @@
+from sqlalchemy.orm import backref
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
 
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=True)
 
+    profile_id = db.Column(db.Integer, db.ForeignKey("profiles.id"))
+    profile = db.relationship(
+        "ProfileModel",
+        uselist=False,
+        lazy="joined",
+        backref=db.backref(
+            "user",
+            lazy="joined",
+        ),
+    )
+
     @property
     def password(self):
-        raise TypeError('A senha não pode ser acessada')
-    
+        raise TypeError("A senha não pode ser acessada")
+
     @password.setter
     def password(self, new_password):
         new_password_hash = generate_password_hash(new_password)
--- demo-20-03-sqlaalchemy/blog/views/post_view.py
+++ demo-20-03-sqlaalchemy/blog/views/post_view.py
@@ -1,4 +1,5 @@
 from flask import request, current_app
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from http import HTTPStatus
 from flask import Blueprint
 
@@ -8,9 +9,11 @@ from blog.models.user_model import UserModel
 bp_post = Blueprint("post_view", __name__, url_prefix="/posts")
 
 
-@bp_post.route("/<int:user_id>", methods=["POST"])
-def create_post(user_id):
+@bp_post.route("/", methods=["POST"])
+@jwt_required()
+def create_post():
     session = current_app.db.session
+    user_id = get_jwt_identity()
 
     body = request.get_json()
     title = body.get("title")
@@ -35,21 +38,57 @@ def create_post(user_id):
     }
 
 
-@bp_post.route("/list/<int:user_id>", methods=["GET"])
-def list_user_posts(user_id):
-    found_user = UserModel.query.get(user_id)
+@bp_post.route("/", methods=["GET"])
+def list_user_posts():
+    try:
+        session = current_app.db.session
+        user_id = get_jwt_identity()
 
-    user_posts = found_user.post_list
+        page = int(request.args.get("page"))
+        per_page = int(request.args.get("per_page"))
 
-    return {
-        "posts": [
-            {"id": post.id, "title": post.title, "content": post.content}
-            for post in user_posts
-        ]
-    }
+        posts_list = (
+            session.query(PostModel, UserModel)
+            .join(UserModel)
+            .filter(UserModel.id == user_id)
+            .paginate(page=page, per_page=per_page, error_out=False)
+            .items
+        )
+
+        if not posts_list:
+            return {"msg": "This page does not exist"}, HTTPStatus.NOT_FOUND
+
+        return {
+            "posts": [
+                {
+                    "nickname": user.nickname,
+                    "id": post.id,
+                    "title": post.title,
+                    "content": post.content,
+                }
+                for post, user in posts_list
+            ]
+        }, HTTPStatus.OK
+
+    except ValueError:
+        return {
+            "msg": "Values in query parametters are not in integer format"
+        }, HTTPStatus.BAD_REQUEST
+
+    # found_user = UserModel.query.get(user_id)
+
+    # user_posts = found_user.post_list
+
+    # return {
+    #     "posts": [
+    #         {"id": post.id, "title": post.title, "content": post.content}
+    #         for post in user_posts
+    #     ]
+    # }
 
 
-@bp_post.route("/delete/<int:post_id>", methods=["DELETE"])
+@bp_post.route("/<int:post_id>", methods=["DELETE"])
+@jwt_required(refresh=True)
 def delete_post(post_id):
     session = current_app.db.session
     found_post = PostModel.query.get(post_id)
