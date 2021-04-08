2c7495c8f9ad49384d676be828247255f9c2583e
Vinicius Abade <vinicius@kenzie.com.br>
2021-04-08 12:11:15 -0300

    demo 08-04 - flask-restful

---
+++ demo_flask_restful/app/configurations/database.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+def init_app(app: Flask):
+    db.init_app(app)
+    app.db = db
+
+    from app.models.track_model import TrackModel
---
+++ demo_flask_restful/app/schemas/__init__.py
@@ -0,0 +1,1 @@
+from app.configurations.serializer import ma
